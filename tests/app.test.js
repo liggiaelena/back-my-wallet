fe5986b373195e037917a3b209ec0b5a57236d67
liggia <liggia.cruz@gmail.com>
2021-10-25 12:17:32 -0300

    test fix bugs

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,9 +3,17 @@ import supertest from 'supertest';
 import connection from '../src/database/database.js';
 
 describe("POST/sing-up",()=>{
+  beforeAll(async()=>{
+    await connection.query(`
+    INSERT INTO users (name, email, password) VALUES ('liggia','abacate@abacate.com','12345');
+    `)
+  })
   afterAll(async()=>{
     await connection.query(`
     DELETE FROM users WHERE email = 'test@test.com';
+    `)
+    await connection.query(`
+    DELETE FROM users WHERE email = 'abacate@abacate.com';
     `)
   })
 
@@ -24,7 +32,7 @@ describe("POST/sing-up",()=>{
     it("returns 409 when email already exist", async() => {
       const body = {
         name: "liggia",
-        email: "abacate@salgado.com",
+        email: "abacate@abacate.com",
         password:"12345"
       };
       const result = await supertest(app).post("/sing-up").send(body);
